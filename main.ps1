7539f0e44de28ec6a5fb17ff6be15b2ad84f3848
Enrico Barbieri <enrico.barbieri.1997@gmail.com>
2024-01-14 14:00:40 +0100

    lockbit method

--- main.ps1
+++ main.ps1
@@ -8,6 +8,54 @@ param(
 $cardsCount = @{}
 $scriptDir = Split-Path -Parent $MyInvocation.MyCommand.Path
 $path = "$scriptDir$path"
+
+$DPI = 300
+$pixelPerCm = $DPI * (1/2.54)
+$cardCmWidth = 5.9
+$cardCmHeight = 8.6
+$A4CmWidth = 21
+$A4CmHeight = 29.7
+$cardsPixelWidth = [Math]::Floor($cardCmWidth * $pixelPerCm)
+$cardsPixelHeight = [Math]::Floor($cardCmHeight * $pixelPerCm)
+$A4Width = [Math]::Floor($A4CmWidth * $pixelPerCm)
+$A4Height = [Math]::Floor($A4CmHeight * $pixelPerCm)
+$horizontalPadding = [Math]::Floor(($A4CmWidth - $cardCmWidth * 3) / 2 * $pixelPerCm)
+$verticalPadding = [Math]::Floor(($A4CmHeight - $cardCmHeight * 3) / 2 * $pixelPerCm)
+
+$page = $null
+$cardsOnCurrentPageCount = 0
+$pageCount = 0
+
+$BitmapConstructorsDeclaration = @"
+using System;
+using System.Drawing;
+public class BitmapConstructors
+{
+  public static Bitmap FromFileWithSize(string filename, Int32 width, Int32 height)
+    {
+        return new Bitmap(Image.FromFile(filename), width, height);
+    }
+  public static Bitmap WithSize(Int32 width, Int32 height)
+    {
+        return new Bitmap(width, height);
+    }
+}
+"@
+
+$BitmapOperationsDeclaration = @"
+using System;
+public class BitmapOperations
+{
+  public static void Copy(System.IntPtr source, System.IntPtr destination, long bytes)
+    {
+        unsafe 
+        {
+            System.Buffer.MemoryCopy((void*)source, (void*)destination, bytes, bytes);
+        }
+    }
+}
+"@
+
 function isNumeric
 {
     param(
@@ -24,6 +72,7 @@ function isNumeric
         return $false
     }
 }
+
 function ParseFileContent
 {
     param(
@@ -44,6 +93,7 @@ function ParseFileContent
         }
     }
 }
+
 function ParseFolder
 {
     param(
@@ -54,6 +104,7 @@ function ParseFolder
         ParseFileContent $_.FullName
     }
 }
+
 function GetCardData
 {
     param (
@@ -73,15 +124,67 @@ function downloadCard
     Invoke-WebRequest $cardUrl -OutFile "$PSScriptRoot$tmpPath$passcode.jpg"
 }
 
+function savePage
+{
+    if($null -eq $page)
+    {
+        return
+    }
+    $page.Save("$PSScriptRoot$($cardsPath)page-$pageCount.png", [System.Drawing.Imaging.ImageFormat]::Png)
+    $global:page = $null
+}
+
 function copyCard
 {
     param (
         [string]$passcode,
         [int]$count
     )
+    $cardBitmap = [BitmapConstructors]::FromFileWithSize("$PSScriptRoot$tmpPath$passcode.jpg", $cardsPixelWidth, $cardsPixelHeight)
+
+    # Lockbit technique
+    $cardRect = New-Object System.Drawing.Rectangle 0, 0, $cardsPixelWidth, $cardsPixelHeight
+    $cardData = $cardBitmap.LockBits($cardRect, [System.Drawing.Imaging.ImageLockMode]::ReadOnly, $cardBitmap.PixelFormat)
+    $cardPointer = $cardData.Scan0
+    
+    $bytes = [Math]::Abs($cardData.Stride) * $cardData.Height
+    # End of lockbit technique
     for ($i = 0; $i -lt $count; $i++) {
-        Copy-Item "$PSScriptRoot$tmpPath$passcode.jpg" -Destination "$PSScriptRoot$cardsPath$passcode-$($i+1).jpg"
+        # Copy-Item "$PSScriptRoot$tmpPath$passcode.jpg" -Destination "$PSScriptRoot$cardsPath$passcode-$($i+1).jpg"
+        if($cardsOnCurrentPageCount -eq 9)
+        {
+            $global:cardsOnCurrentPageCount = 0
+            $global:pageCount += 1
+            savePage
+        }
+        if($null -eq $page)
+        {
+            $global:page = [BitmapConstructors]::WithSize($A4Width, $A4Height)
+            $page.SetResolution($DPI, $DPI)
+        }
+        $row = [Math]::Floor($cardsOnCurrentPageCount / 3)
+        $col = $cardsOnCurrentPageCount % 3
+        $x = [Math]::Floor($horizontalPadding + $col * $cardsPixelWidth)
+        $y = [Math]::Floor($verticalPadding + $row * $cardsPixelHeight)
+
+        # Lockbit technique
+        $pageRect = New-Object System.Drawing.Rectangle $x, $y, $cardsPixelWidth, $cardsPixelHeight
+        $pageData = $page.LockBits($pageRect, [System.Drawing.Imaging.ImageLockMode]::ReadWrite, $page.PixelFormat)
+        $pagePointer = $pageData.Scan0
+
+        # $temp = New-Object byte[] $bytes
+        #[System.Runtime.InteropServices.Marshal]::Copy($cardPointer, $temp, 0, $bytes)
+        #[System.Runtime.InteropServices.Marshal]::Copy($temp, 0, $pagePointer, $bytes)
+        [BitmapOperations]::Copy($cardPointer, $pagePointer, $bytes)
+
+        $page.UnlockBits($pageData)
+        # End of lockbit technique
+        
+        $global:cardsOnCurrentPageCount += 1
     }
+    # Lockbit technique
+    $cardBitmap.UnlockBits($cardData)
+    # End of lockbit technique
 }
 
 function WriteDeckList
@@ -93,8 +196,12 @@ function WriteDeckList
     {
         downloadCard $c.Key
         copyCard $c.Key $c.Value
-    }    
+    }
+    savePage
 }
+Add-Type -AssemblyName System.Drawing
+Add-Type -TypeDefinition $BitmapConstructorsDeclaration -ReferencedAssemblies System.Drawing.Common,System.Drawing.Primitives
+Add-Type -TypeDefinition $BitmapOperationsDeclaration -CompilerOptions '/unsafe'
 ParseFolder $path
 WriteDeckList $cardsCount
 "End"
